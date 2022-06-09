7cfe2262ea5ec94661dfb1af9a3cd832b96843de
nocyphr <nocyphr@outlook.com>
2022-06-09 17:30:33 +0200

    generate initial ISIN:SYMBOL list

--- dataGrabbers.py
+++ dataGrabbers.py
@@ -14,6 +14,7 @@ class Onvista:
     def getStockList(self, csvName = False):
         if csvName:
             self.bigPanda.pdStocks = pd.read_csv(csvName, index_col = None)
+
         if not csvName:
             urlList = [self.url + str(_) for _ in range(1, self.results.maxResults + 50, 50)]
             pandaList = MultiProcessor(self.buildPandas, urlList).resultsList
@@ -47,10 +48,34 @@ class PandaStocks:
         self.pdStocks.drop(['figures', 'last', 'nsin', 'date'], axis = 1, inplace = True)
 
 
+class Yahoo:
+    def __init__(self, isinList = False):
+        if isinList:
+            self.searchUrlList = [(f'https://query2.finance.yahoo.com/v1/finance/search?q={isin}&newsQueryId=news_cie_vespa', isin) for isin in isinList]
+        self.resultsList = []
+
+    def builtResults(self, args):
+        page = WebPage(args[0], 60)
+        quotes = page.getJSON()['quotes']
+        print(f'working on {args[1]}')
+        result = [args[1], False]
+        if len(quotes):
+            result = [args[1], quotes[0]['symbol']]
+        return result
+
+    def getSymbols(self):
+        self.resultsList = MultiProcessor(self.builtResults, self.searchUrlList, poolsize = 30).resultsList
+
+    def getCSV(self, csvName = 'isinSymbolList.csv'):
+        pd.DataFrame(self.resultsList).to_csv(csvName, index = False)
 
 # o = Onvista()
 # o.getStockList()
 # o.getCSV()
 o = Onvista()
 o.getStockList('stocksList.csv')
-print(o.bigPanda.pdStocks)
+t = [i for i in o.bigPanda.pdStocks['isin']]
+y = Yahoo(t)
+y.getSymbols()
+print(y.resultsList)
+y.getCSV()
