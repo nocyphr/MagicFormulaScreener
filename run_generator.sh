0d3dff39afa11fe28e5a32293418b886426fbdc5
nocyphr <nocyphr@outlook.com>
2022-06-16 13:35:45 +0200

    added next steps like removing duplicates and cleanup

--- run_generator.sh
+++ run_generator.sh
@@ -1,6 +1,21 @@
 cd src
-python grabOnvista.py
+echo 'now grabbing stocksList'
+python -c 'from grabOnvista import Onvista; Onvista().generate_stocks_list()'
+echo 'switching to Data-Directory'
 cd ../Data
+echo 'removing duplicate entries in stocksList.csv'
 (head -n 1 stocksList.csv && tail -n +3 stocksList.csv | sort -u) > stocksList2.csv
+echo 'cleanup'
 rm stocksList.csv
 mv stocksList2.csv stocksList.csv
+echo 'switching to src-Directory'
+cd ../src
+echo 'now translating ISIN to Symbol'
+python -c 'from grabYahooSymbol import Yahoo; Yahoo().get_symbols()'
+echo 'switching to Data-Directory'
+cd ../Data
+echo 'removing duplicate entries in isinSymbolList.csv'
+(head -n 1 isinSymbolList.csv && tail -n +3 isinSymbolList.csv | sort -u) > isinSymbolList2.csv
+echo 'cleanup'
+rm isinSymbolList.csv
+mv isinSymbolList2.csv isinSymbolList.csv
