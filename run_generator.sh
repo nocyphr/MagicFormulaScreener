fbaa305d5fb58ec8fddf3417a774536f43850f5f
nocyphr <nocyphr@outlook.com>
2022-06-16 14:40:28 +0200

    generator is done for now

--- run_generator.sh
+++ run_generator.sh
@@ -19,3 +19,17 @@ echo 'removing duplicate entries in isinSymbolList.csv'
 echo 'cleanup'
 rm isinSymbolList.csv
 mv isinSymbolList2.csv isinSymbolList.csv
+echo 'switching to src-Directory'
+cd ../src
+echo 'joining isinSymbolList to stocksList on column isin'
+python -c 'from mutateData import Mutate; Mutate(csvName="stocksList.csv").join_on_column("isinSymbolList.csv", "isin")'
+echo 'switching to Data-Directory'
+cd ../Data
+echo 'removing entries without ticker symbol from stockslist'
+sed -i '/False/d' ./stocksList.csv
+echo 'removing duplicate entries in stocksList.csv'
+(head -n 1 stocksList.csv && tail -n +3 stocksList.csv | sort -u) > stocksList2.csv
+echo 'cleanup'
+rm stocksList.csv
+mv stocksList2.csv stocksList.csv
+echo 'Done'
