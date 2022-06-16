0391f7cd48d2e5a99c01ff18bf2e72a91673d7c7
nocyphr <nocyphr@outlook.com>
2022-06-16 13:38:10 +0200

    fixed save_data method, extracted object instantiation into bashfile

--- src/grabYahooSymbol.py
+++ src/grabYahooSymbol.py
@@ -4,6 +4,7 @@ from mutateData import Mutate
 
 
 
+
 class Yahoo:
     def __init__(self):
         self.parameters = YahooParameters()
@@ -30,10 +31,11 @@ class Yahoo:
             0: 'isin',
             1: 'symbol'
         }
-        append_panda = Mutate(panda=append_panda, read=False).rename_panda_columns(rename_columns_dictionary)
+        append_panda = Mutate(panda=append_panda, read=False)
+        append_panda.rename_panda_columns(rename_columns_dictionary)
 
-        joined_panda = base_panda.join_pandas([append_panda])
-        joined_panda = Mutate(symbol_file_name, joined_panda, False).panda
+        joined_panda = base_panda.join_pandas([append_panda.panda])
+        joined_panda = Mutate(symbol_file_name, joined_panda, False)
 
         joined_panda.make_csv()
 
@@ -46,13 +48,13 @@ class Yahoo:
         symbol_dictionary = self.parameters.symbol_dictionary()
 
         if isin not in isin_list:
-            print(isin)
+
             page = WebPage(url, 120)
             symbol_exists = page.getJSON()['quotes']
+            print(f'{isin}: {symbol_exists}')
             return [isin, (symbol_exists[0]['symbol'] if symbol_exists else False)]
 
         symbol = symbol_dictionary[isin]
-        print(symbol)
         return [isin, symbol]
 
 
